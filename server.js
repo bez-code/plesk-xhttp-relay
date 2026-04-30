9b2c83551d0367fe5ccecd2f961ee0285c735a4a
Behzad Ashrafi <behzad.p.ashrafi@gmail.com>
2026-04-30 15:36:33 +0330

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/health") {
-    res.writeHead(204, {
+    res.writeHead(200, {
+      "content-type": "text/plain; charset=utf-8",
       "cache-control": "no-store"
     });
-    res.end();
+    res.end("health ok");
     return;
   }
 
